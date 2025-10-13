584cc3097459b1956a19c8154af919bb14fb831c
Linuss <aryan2003khare@gmail.com>
2025-10-14 02:26:17 +0530

    made changes in ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react"
 import { PlaylistGenerator } from "./components/PlaylistGenerator"
-import { Navbar } from "./components/Navbar"
+import { Navbar } from "./components/Navbar";
+import vinyl from "./assets/BenBois_Vinyl_records.svg";
+import vinyl3 from "./assets/f1.png";
+import vinyl2 from "./assets/f3.webp";
 
 function App() {
 
@@ -121,59 +124,79 @@ return (
         background: "radial-gradient(ellipse 80% 60% at 50% 0%, rgba(16, 185, 129, 0.25), transparent 70%), #000000",
       }}
     />
+
+    
   
     {/* Your Content/Components */}
 
     <Navbar />
 
+           <div className="relative left-32 z-20 w-px h-full bg-gradient-to-b from-neutral-300 via-neutral-200 to-transparent inset-y-0 " />
+
+
 
            {/* main container  */}
-      <div className="relative  max-w-6xl  min-h-screen flex flex-col gap-4 border mx-auto">
+      <div className="relative max-w-6xl min-h-screen flex flex-col gap-4  mx-auto">
+        
 
-        <div className="w-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/21 h-32 bg-gradient-to-r from-transparent via-green-600/40 to-transparent blur-3xl"/>
+           <div className="absolute left-24 w-px h-full bg-gradient-to-b from-transparent via-neutral-800 to-transparent inset-y-0 " />
+           <div className="absolute right-24 w-px h-full bg-gradient-to-b from-transparent via-neutral-800 to-transparent inset-y-0 " />
+
+
+        {/* <div className="w-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/21 h-32 bg-gradient-to-r from-transparent via-green-600/40 to-transparent blur-3xl"/> */}
 
       {!token ? (
 
         // treating this as a whole compo.
         // div container for these things
-        <div className=" max-w-6xl mt-20 flex flex-col items-center gap-6 mx-auto p-4 border-white ">
+        <div className=" max-w-6xl mt-8 flex flex-col font-sans items-center gap-4 mx-auto p-4 border-white  ">
 
           {/* div for heading  */}
-          <div className=" border-neutral-300 font-mono mb-10 text-neutral-400 ">
-            <h1 className="text-6xl text-center ">
-              Let AI handle <br /> your,<span className="text-neutral-800">Playlists</span>
+          <div className=" border-neutral-300 text-neutral-400 ">
+            <h1 className="text-6xl text-center bg-gradient-to-r from-neutral-300 via-neutral-500 to-neutral-700 bg-clip-text text-transparent">
+              Find Your Playlist
             </h1>
           </div>
 
+          <h3 className="text-3xl text-neutral-300 mt-2">More than just Music</h3>
+
           {/* contaier for the BOX parts of the design */}
-          <div className=" w-full bg-gradient-to-b from-black/10 to-red-950/40 backdrop-blur-lg gap-10 rounded-3xl text-neutral-200 flex flex-col items-center px-6 py-4 ">
-            <p className="text-center mt-10 ">
-                    Let AI create the perfect playlist for any mood <br /> 
-      Just describe it here what you  <br /> in the prompt what want 
+          {/* <div className=" w-full bg-gradient-to-b from-black/10 to-red-950/40 backdrop-blur-lg gap-10 rounded-3xl text-neutral-200 flex flex-col items-center px-6 py-4 "> */}
+          {/* </div> */}
+
+            <p className="text-center mt-2 text-base leading-relaxed text-neutral-300 ">
+                    Let AI create the perfect playlist for any<br /> 
+      Just describe it what you 
             </p>
 
-            <div className=" py-4 px-4 flex gap-4">
+            <div className="">
               {/* first button  */}
-              <button className="px-4 py-1 rounded-2xl text-md  bg-neutral-900 text-neutral-300 "
+              <button className="px-8 py-1 mt-2 rounded-lg text-md  bg-green-900 text-neutral-200 cursor-pointer shadow-lg "
               onClick={handleLogin}
               >
-                Get Started
-              </button>
-              <button className="px-4 py-1 rounded-2xl text-sm   bg-neutral-300 text-neutral-950 ">
-                Watch Video
+                Login with Spotify
               </button>
             </div>
 
-            <div className="w-full border-t border-neutral-400" />
+            <div className="w-sm mt-4 border-t border-neutral-500" />
 
-            <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-6 pb-8 ">
+            {/* gridd for that old ui but ig not needed now maybe */}
+            {/* <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-6 pb-8 ">
               <div className=""></div>
               <div className=""></div>
+            </div> */}
+
+            {/* div for the image */}
+            <div className=" border-white 2 max-w-7xl mx-auto  ">
+              <img 
+              src={vinyl2}
+              alt="vinyl-img" 
+              height={700}
+              width={700}
+              className="object-contain  mask-b-from-20% mask-b-to-80% " />
             </div>
 
-          </div>
-
-
+            
 
         </div>
 
@@ -200,5 +223,7 @@ return (
 }
 
 export default App
+
+
 
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,9 @@
 
 export const Navbar = () => {
   return (
-    <nav className="relative z-10  max-w-4xl mx-auto py-4 px-2 ">
+    <nav className="relative z-10  max-w-4xl mx-auto pt-2 px-2 ">
         {/* container div for the children */}
-        <div className=" border border-neutral-500 text-neutral-200 font-mono rounded-full flex justify-between items-center px-6 py-3">
+        <div className=" border-b-0  border-neutral-500 text-neutral-200 font-mono rounded-full flex justify-between items-center px-6 py-3">
             {/* first logo  */}
             <div className="font-semitb  font-mono text-lg ">
                 Playlisteee
